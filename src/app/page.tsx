2fdff84d8074170af66f5164cb9334635a1bb975
shulmnn <matvejsh17@gmail.com>
2024-11-12 13:50:26 +0300

    move hook to the parent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 "use client";
 
+import { useEffect } from "react";
 import Microphone from "~/components/Microphone";
 import { useRecordVoice } from "~/hooks/useRecordVoice";
 
 export default function Page() {
-  const { text } = useRecordVoice();
+  const { startRecording, stopRecording, text } = useRecordVoice();
+
   return (
     <div className="flex min-h-dvh flex-col p-4">
       <h1>Seekr voice helper</h1>
       <div className="flex flex-grow items-center justify-center">{text}</div>
       <div className="flex h-12 items-center justify-center">
-        <Microphone />
+        <Microphone
+          startRecording={startRecording}
+          stopRecording={stopRecording}
+        />
       </div>
     </div>
   );
--- src/components/Microphone.tsx
+++ src/components/Microphone.tsx
@@ -1,18 +1,24 @@
 "use client";
 
+import { useEffect } from "react";
 import { useRecordVoice } from "~/hooks/useRecordVoice";
 
-export default function Microphone() {
-  const { startRecording, stopRecording } = useRecordVoice();
+interface MicrophoneProps {
+  startRecording: () => void;
+  stopRecording: () => void;
+}
 
+export default function Microphone({
+  startRecording,
+  stopRecording,
+}: MicrophoneProps) {
   return (
-    // Button for starting and stopping voice recording
     <>
       <button
-        onMouseDown={startRecording} // Start recording when mouse is pressed
-        onMouseUp={stopRecording} // Stop recording when mouse is released
-        onTouchStart={startRecording} // Start recording when touch begins on a touch device
-        onTouchEnd={stopRecording} // Stop recording when touch ends on a touch device
+        onMouseDown={startRecording}
+        onMouseUp={stopRecording}
+        onTouchStart={startRecording}
+        onTouchEnd={stopRecording}
         className="select-none rounded-full border-none bg-neutral-200 bg-transparent px-4 py-2 text-neutral-800 active:bg-red-600 active:text-white"
       >
         push to talk
